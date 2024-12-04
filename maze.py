811e80a500ac3812a7c28fb7d3387d7c8fc54e4a
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-12-04 18:47:25 +0400

    add _reset_cells_visited

--- maze.py
+++ maze.py
@@ -30,6 +30,7 @@ class Maze():
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -100,4 +101,9 @@ class Maze():
             self._draw_cell(next_cell_info[0][0], next_cell_info[0][1])
 
             self._break_walls_r(next_cell_info[0][0], next_cell_info[0][1])
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
             
